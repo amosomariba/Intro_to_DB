335ff642dcb6fe6339114ae46fe9397086591493
Amos Omariba <amomariba@gmail.com>
2024-07-28 23:06:38 +0300

    add alx_book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,17 +1,17 @@
--- Creating the alx_book_store database
-CREATE DATABASE alx_book_store;
+-- Creating the alx_book_store database if it does not exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Using the alx_book_store database
 USE alx_book_store;
 
 -- Creating the Authors table
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Creating the Books table
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE Books (
 );
 
 -- Creating the Customers table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE Customers (
 );
 
 -- Creating the Orders table
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -37,7 +37,7 @@ CREATE TABLE Orders (
 );
 
 -- Creating the Order_Details table
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
