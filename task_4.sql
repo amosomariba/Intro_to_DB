4ea0dd4041b0b5baa6f75eb66acfc720e9957933
Amos Omariba <amomariba@gmail.com>
2024-07-28 23:50:44 +0300

    create a script that prints full description of the table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,7 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
 -- Print the full description of the table books from the database alx_book_store
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.columns
-WHERE table_schema = 'alx_book_store' AND table_name = 'Books';
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
